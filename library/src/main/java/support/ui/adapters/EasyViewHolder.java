7d4d8258d66583f96ad8dd8630b833216c4ceb64
杨玉刚 <smartydroid@gmail.com>
2016-04-18 23:18:55 +0800

    add adapter for recycler view

---
+++ library/src/main/java/support/ui/adapters/EasyViewHolder.java
@@ -0,0 +1,54 @@
+package support.ui.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public abstract class EasyViewHolder<V> extends RecyclerView.ViewHolder
+    implements View.OnLongClickListener, View.OnClickListener {
+
+  private OnItemClickListener itemClickListener;
+  private OnItemLongClickListener longClickListener;
+
+  public EasyViewHolder(Context context, ViewGroup parent, int layoutId) {
+    super(LayoutInflater.from(context).inflate(layoutId, parent, false));
+    bindListeners();
+  }
+
+  void setItemClickListener(OnItemClickListener itemClickListener) {
+    this.itemClickListener = itemClickListener;
+  }
+
+  void setLongClickListener(OnItemLongClickListener longClickListener) {
+    this.longClickListener = longClickListener;
+  }
+
+  private void bindListeners() {
+    itemView.setOnClickListener(this);
+    itemView.setOnLongClickListener(this);
+  }
+
+  @Override public boolean onLongClick(View v) {
+    if (longClickListener == null) {
+      return false;
+    }
+    return longClickListener.onLongItemClicked(getPosition(), itemView);
+  }
+
+  @Override public void onClick(View v) {
+    if (itemClickListener == null) return;
+    itemClickListener.onItemClick(getAdapterPosition(), v);
+  }
+
+  public abstract void bindTo(V value);
+
+  public interface OnItemClickListener {
+    void onItemClick(final int position, View view);
+  }
+
+  public interface OnItemLongClickListener {
+    boolean onLongItemClicked(final int position, View view);
+  }
+}
